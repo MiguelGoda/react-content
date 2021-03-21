11bade11e20d2dd3afa11d7c417ddffd2b28f925
MiguelGoda <miguel_g14@hotmail.com>
2021-03-21 02:04:53 -0400

    segundo commit agregando proyecto

---
+++ src/context/Users/UserState.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserState = {
+  users: [],
+  selectedUser: null,
+};
+
+const getUsers = () => {};
+
+const getProfile = () => {};
+
+export default UserState;
